27eceff3668cc29ae6b1cebb825feb2bc1f5aa67
afzal75 <afzalbhuiyan16@gmail.com>
2024-01-26 23:10:58 +0600

    movie card created successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import MovieList from "./cine/MovieList";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           <Sidebar />
+          <MovieList />
         </div>
       </main>
     </>
